7d78449dda4a9b2c8d75766f2313591b7d068999
Hiba Siddiqui <hibasidd2008@gmail.com>
2024-07-31 16:13:14 +0500

    Add files via upload

---
+++ clock using tkinter.py
@@ -0,0 +1,13 @@
+from tkinter import *
+import time
+root = Tk()
+root.geometry("400x200")
+root.config(bg="black")
+def update():
+    clock.config(text=time.strftime("%I:%M:%S"))
+    clock.after(1000,update)
+clock = Label(root,background="black",foreground="white",font=("arial",40,"bold"))
+clock.pack()
+update()
+root.title("Digital Clock Using Tkinter")
+root.mainloop()
